81e7e9f2f5ef9289d41d3b4f31339ed732a4be2d
SALAMA KASSIM <salama.kassim@pau.edu.ng>
2026-05-23 08:54:09 +0100

    Implemented CSV data ingestion function

--- main.py
+++ main.py
@@ -27,3 +27,21 @@ class UserProfile:
         if self.budget > 0 and self.industry != "" and self.density_pref != "":
             return True
         return False
+
+
+# 2. DATA HANDLING
+def load_locations(filename):
+    locations_list = []
+    try:
+        with open(filename, mode='r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                if len(row) == 5:
+                    new_loc = Location(row[0], row[1], row[2], row[3], row[4])
+                    locations_list.append(new_loc)
+            print("Success: Database loaded.")
+    except FileNotFoundError:
+        print(f"Error: Could not find {filename}")
+    return locations_list
+    
